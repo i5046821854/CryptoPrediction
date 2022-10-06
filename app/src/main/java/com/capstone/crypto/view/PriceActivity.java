ed84cbbb28d6c0cd7cad641e0b039124542eb112
machinaa <i5046821854@github.com>
2022-10-07 01:47:08 +0900

    #3 - 가상화폐 가격을 MPAndroid를 이용하여 라인차트로 표현

--- app/src/main/java/com/capstone/crypto/view/PriceActivity.java
+++ app/src/main/java/com/capstone/crypto/view/PriceActivity.java
@@ -1,6 +1,7 @@
 package com.capstone.crypto.view;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.capstone.crypto.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -30,13 +38,17 @@ public class PriceActivity  extends AppCompatActivity {
 
     EditText cryptoTxt;
     Button searchBtn;
-
+    LineChart chart;
+    Thread thread;
+    int i = 0;
+    List<CryptoPrice> cryptoCurrencies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_price);
         searchBtn = findViewById(R.id.searchBtn2);
         cryptoTxt = findViewById(R.id.searchBox);
+        chart = findViewById(R.id.bar);
         initView();
         searchBtn.setOnClickListener(view -> {
             String crypto = cryptoTxt.getText().toString();
@@ -51,8 +63,93 @@ public class PriceActivity  extends AppCompatActivity {
         cryptoTxt.setText(name);
         searchPrice(name);
         searchRealPrice(name);
+
     }
 
+    void drawLineChart()
+    {
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setDrawGridLines(false);
+        YAxis leftAxis = chart.getAxisRight();
+        leftAxis.setDrawGridLines(false);
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+        LineData data = new LineData();
+        chart.setData(data);
+        feedMultiple();
+    }
+    void feedMultiple()
+    {
+        if(thread != null)
+            thread.interrupt();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                addEntry();
+            }
+        };
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+//                while(true)
+//                {
+//                    if(i == 2400)
+//                    {
+//                        i = 0;
+//                        break;
+//                    }
+//                    runOnUiThread(runnable);
+//                    i++;
+//                    try {
+//                        Thread.sleep(1);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+                runOnUiThread(runnable);
+            }
+        });
+        thread.start();
+    }
+
+    void addEntry() {
+        System.out.println(i);
+        LineData data = chart.getData();
+        LineDataSet set = (LineDataSet) data.getDataSetByIndex(0);
+        if (set == null) {
+            set = createSet();
+            data.addDataSet(set);
+        }
+        while (true) {
+            if (i == 2400) {
+                i = 0;
+                break;
+            }
+            data.addEntry(new Entry(i, (float) cryptoCurrencies.get(i).getHigh()), 0);
+            i++;
+        }
+        data.notifyDataChanged();
+        chart.notifyDataSetChanged();
+        chart.setVisibleXRangeMaximum(1000);
+        chart.moveViewToX(data.getEntryCount());
+
+    }
+
+    private LineDataSet createSet()
+    {
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(ColorTemplate.getHoloBlue());
+        set.setLineWidth(4f);
+        set.setCircleRadius(0.1f);
+        set.setFillAlpha(95);
+        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setHighLightColor(Color.rgb(244,177,177));
+        set.setDrawValues(false);
+        return set;
+    }
     void searchRealPrice(String name)
     {
         String crypto = name.toLowerCase(Locale.ROOT);
@@ -79,12 +176,9 @@ public class PriceActivity  extends AppCompatActivity {
                 Gson gson = new GsonBuilder().create();
                 System.out.println(myResponse);
                 Type collectionType = new TypeToken<List<CryptoPrice>>(){}.getType();
-                List<CryptoPrice> cryptoCurrencies = (List<CryptoPrice>) new Gson()
+                cryptoCurrencies = (List<CryptoPrice>) new Gson()
                         .fromJson( myResponse , collectionType);
-                for(CryptoPrice crypto : cryptoCurrencies)
-                {
-                    System.out.println(crypto.getTime() + crypto.getClose());
-                }
+                drawLineChart();
             }
         });
 
